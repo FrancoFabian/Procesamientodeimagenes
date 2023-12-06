14c0d755efc77afe5a4f6fa9fe4538b68776c2bf
FrancoFabian <jeralfabian489@gmail.com>
2023-12-06 13:58:03 -0600

    AspectoImage,renderizarcanvasImage

--- src/components/CanvasImage.tsx
+++ src/components/CanvasImage.tsx
@@ -7,21 +7,28 @@ interface CanvasComponentState {
     height: number;
   };
 }
+interface CanvasImageProps {
+  dimensions: {
+    width: number;
+    height: number;
+  };
+}
 
-class CanvasImage extends Component<{}, CanvasComponentState> {
+class CanvasImage extends Component<CanvasImageProps, CanvasComponentState> {
   static contextType = ModalContext;
+  private prevImage: string | null = null;
   // Usar `!` para aserción de no nulidad, eliminando la necesidad de `declare` aquí.
  declare context: React.ContextType<typeof ModalContext>;
 
   private canvasRef: React.RefObject<HTMLCanvasElement>;
 
-  constructor(props: {}) {
+  constructor(props: CanvasImageProps) {
     super(props);
     this.canvasRef = React.createRef<HTMLCanvasElement>();
     this.state = {
       dimensions: {
-        width: 0,
-        height: 0
+        width: props.dimensions.width,
+        height: props.dimensions.height
       }
     };
   }
@@ -29,26 +36,61 @@ class CanvasImage extends Component<{}, CanvasComponentState> {
  
   componentDidMount() {
     this.updateCanvasDimensions(this.context!.image);
+    this.prevImage = this.context!.image;
   }
 
+ /* componentDidUpdate(prevProps: CanvasImageProps) {
+    if (prevProps.dimensions.width !== this.props.dimensions.width || 
+        prevProps.dimensions.height !== this.props.dimensions.height) {
+      this.setState({ dimensions: this.props.dimensions });
+      this.updateCanvasDimensions(this.context!.image);
+    }
+  }*/
   componentDidUpdate() {
-    // Si la URL de la imagen en el contexto cambia, actualiza el canvas.
-    this.updateCanvasDimensions(this.context!.image);
+    const { image } = this.context!;
+    const dimensionsChanged = this.props.dimensions.width !== this.state.dimensions.width ||
+                              this.props.dimensions.height !== this.state.dimensions.height;
+    
+    if (image !== this.prevImage || dimensionsChanged) {
+      this.updateCanvasDimensions(image);
+      this.prevImage = image;
+    }
   }
 
+ 
   updateCanvasDimensions = (imageSrc: string | null) => {
     if (imageSrc) {
       const img = new Image();
       img.onload = () => {
         const { width, height } = img;
-        this.setState({ dimensions: { width, height } }, () => {
+        const maxWidth = this.state.dimensions.width;
+        const maxHeight = this.state.dimensions.height;
+        
+        let newWidth:number, newHeight:number;
+  
+        // Calcular la relación de aspecto de la imagen y del canvas
+        const imgAspectRatio = width / height;
+        const canvasAspectRatio = maxWidth / maxHeight;
+  
+        if (imgAspectRatio > canvasAspectRatio) {
+          // Si la imagen es más ancha en proporción al canvas
+          newWidth = maxWidth;
+          newHeight = maxWidth / imgAspectRatio;
+        } else {
+          // Si la imagen es más alta en proporción al canvas
+          newHeight = maxHeight;
+          newWidth = maxHeight * imgAspectRatio;
+        }
+  
+        this.setState({ dimensions: { width: newWidth, height: newHeight } }, () => {
           const canvas = this.canvasRef.current;
           if (canvas) {
-            canvas.width = width;
-            canvas.height = height;
+            canvas.width = newWidth;
+            canvas.height = newHeight;
             const ctx = canvas.getContext('2d');
             if (ctx) {
-              ctx.drawImage(img, 0, 0);
+              ctx.clearRect(0, 0, newWidth, newHeight);
+              ctx.drawImage(img, 0, 0, newWidth, newHeight);
             }
           }
         });
@@ -56,15 +98,16 @@ class CanvasImage extends Component<{}, CanvasComponentState> {
       img.src = imageSrc;
     }
   };
+  
 
   render() {
+    const { width, height } = this.state.dimensions;
     return (
       <canvas
         ref={this.canvasRef}
         style={{
-          width: `${this.state.dimensions.width}px`,
-          height: `${this.state.dimensions.height}px`,
-          border: '1px solid black' // Opcional para visualización
+          width: `${width}px`,
+          height: `${height}px`
         }}
       />
     );
--- src/components/CanvasPro.tsx
+++ src/components/CanvasPro.tsx
@@ -1,16 +1,43 @@
-import React from "react";
-import "./styles/CanvasPro.css"
-import CanvasImage from "./CanvasImage";
+import React, { useEffect, useRef, useState, useContext } from 'react';
+import './styles/CanvasPro.css';
+import CanvasImage from './CanvasImage';
+import { ModalContext } from './context/ModalProvider';
 
-const CanvasPro:React.FC =  ()=>{
-  
+const CanvasPro: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const modalContext = useContext(ModalContext);
 
+  useEffect(() => {
+    const updateDimensions = () => {
+      if (containerRef.current) {
+        const { width, height } = containerRef.current.getBoundingClientRect();
+        setDimensions({ width, height });
+      }
+    };
 
-   return(
-    <div className="ContenedorCnavas">
-      <CanvasImage/>
+    updateDimensions();
+
+    const resizeObserver = new ResizeObserver(updateDimensions);
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    return () => resizeObserver.disconnect();
+  }, []);
+
+  useEffect(() => {
+    if (modalContext?.image && dimensions.width && dimensions.height) {
+      // Este efecto se activa cuando la imagen cambia y las dimensiones son válidas.
+    }
+  }, [modalContext?.image, dimensions]);
+
+  return (
+    <div ref={containerRef} className="ContenedorCnavas">
+      {dimensions.width > 0 && dimensions.height > 0 && (
+        <CanvasImage dimensions={dimensions} />
+      )}
     </div>
-    
-   );
-}
+  );
+};
 export default CanvasPro;
